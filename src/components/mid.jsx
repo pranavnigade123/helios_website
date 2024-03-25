8538b8a07f18ea0a75946b2baac8352c0474e038
Pranav Nigade <nigades97@gmail.com>
2024-03-26 00:49:29 +0530

    added animations to homepage

--- src/components/mid.jsx
+++ src/components/mid.jsx
@@ -1,21 +1,24 @@
-const Mid = () => {
-    return (
-        <main className="mid container">
-            <div className="mid-content">
-                <h1 id="title1">HELIOS: Powering the Crypto Cosmos</h1>
-                <p id="content1">HELIOS fuels an expansive ecosystem, empowering developers to scale apps across blockchains.
-                 As our foundation, HELIOS ensures security and scalability, fostering a robust platform.</p>
-                 
-                 <div className="mid-btn">
-                  <button className="explore-btn">Explore Data</button>
-                  <button className="start-btn">Get Started</button>
-                </div>
+import React from 'react';
+import './Mid.css'; // Import CSS file for styling
 
-            </div>
-            <div className="mid-image">
-            <img src="images\helios_mid3.jpg" alt="mid-img"/>
-            </div>
-        </main>
-    );
+const Mid = () => {
+  return (
+    <main className="mid container">
+      <div className="mid-content fade-in-left">
+        <h1 id="title1">HELIOS: Powering the Crypto Cosmos</h1>
+        <p id="content1">HELIOS fuels an expansive ecosystem, empowering developers to scale apps across blockchains.
+          As our foundation, HELIOS ensures security and scalability, fostering a robust platform.</p>
+
+        <div className="mid-btn">
+          <button className="explore-btn">Explore Data</button>
+          <button className="start-btn">Get Started</button>
+        </div>
+      </div>
+      <div className="mid-image fade-in-right">
+        <img src="images\helios_mid3.jpg" alt="mid-img" />
+      </div>
+    </main>
+  );
 };
-export default Mid
+
+export default Mid;
