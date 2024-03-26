4b4d9b309bba3e8742fbd0710679cb94434ac290
Pranav Nigade <nigades97@gmail.com>
2024-03-26 19:35:28 +0530

    added a hamburger menu and made it more responsive

--- src/components/mid.jsx
+++ src/components/mid.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Mid.css'; // Import CSS file for styling
 
 const Mid = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768); // Adjust the width threshold as per your definition of smaller screens
+    };
+
+    handleResize(); // Call once on initial render
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <main className="mid container">
-      <div className="mid-content fade-in-left">
+      <div className={`mid-content ${isSmallScreen ? 'slide-up' : ''}`}>
         <h1 id="title1">HELIOS: Powering the Crypto Cosmos</h1>
         <p id="content1">HELIOS fuels an expansive ecosystem, empowering developers to scale apps across blockchains.
           As our foundation, HELIOS ensures security and scalability, fostering a robust platform.</p>
@@ -14,7 +29,7 @@ const Mid = () => {
           <button className="start-btn">Get Started</button>
         </div>
       </div>
-      <div className="mid-image fade-in-right">
+      <div className={`mid-image ${isSmallScreen ? 'slide-up' : 'fade-in-right'}`}>
         <img src="images\helios_mid3.jpg" alt="mid-img" />
       </div>
     </main>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,16 +1,33 @@
+import React, { useState } from 'react';
+
 const Navigation = () => {
-    return (
-<nav className="container">
-        <div className="logo">
-          <img src="images/brandlogo.svg" alt="logo"/>
-        </div>
-        <ul>
-          <li><a href="#">Solutions</a></li>
-          <li><a href="#">Research</a></li>
-          <li><a href="#">About Us</a></li>
-        </ul>
-        <input type="text" placeholder="Search..." />
-      </nav>
-    );
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // Function to toggle mobile menu
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="images/brandlogo.svg" alt="logo" />
+      </div>
+      {/* Hamburger menu icon */}
+      <div className={`hamburger-menu ${mobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
+      <div className="line line-1"></div>
+      <div className="line line-2"></div>
+      <div className="line line-3"></div>
+      </div>
+      {/* Navigation links */}
+      <ul className={`mobile-menu ${mobileMenuOpen ? 'active' : ''}`}>
+        <li><a href="#">Solutions</a></li>
+        <li><a href="#">Research</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+      <input type="text" placeholder="Search..." />
+    </nav>
+  );
 };
-export default Navigation
+
+export default Navigation;
